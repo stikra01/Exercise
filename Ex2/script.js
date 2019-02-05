53a85be0b52e3a5aad553589498df43661afd352
Daniel Dev <stikra@gmail.com>
2019-02-05 04:31:27 +0100

    Exercise 1 5luty19

---
+++ Ex2/script.js
@@ -0,0 +1,9 @@
+var liczba1 =  prompt("podaj liczbę ");
+var liczba2 = prompt("podaj liczbę 2");
+if (liczba1 > liczba2) {
+  document.write("<b>Liczba 1 jest większa</b>");
+} else if (liczba2 > liczba1) {
+  document.write("<b>Liczba 2 jest większa</b>");
+} else {
+  document.write("<b>Liczby są równe</b>");
+}
