de24197291c849d63fea0415d9f80d8ae105b502
Daniel Dev <41228671+stikra01@users.noreply.github.com>
2019-02-18 16:58:02 +0100

    Create script.js

---
+++ Dopa/script.js
@@ -0,0 +1,14 @@
+var n = 12;
+
+for (var i = 0; i <= n; i++){
+	for (var j = 0; j<= n; j++){
+		console.log("i= " + i + ", j= " + j);
+	}
+}var n = 12;
+
+for (var i = 0; i <= n; i++){
+	for (var j = 0; j<= n; j++){
+		console.log("i= " + i + ", j= " + j);
+	}
+}
+//dopa
